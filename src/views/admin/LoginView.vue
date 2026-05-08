cd2b697ed3c5c7ace7debc463e56fe60c0618882
tegark524 <tegark525@gmail.com>
2026-05-08 16:02:35 +0700

    login v3

--- src/views/admin/LoginView.vue
+++ src/views/admin/LoginView.vue
@@ -76,6 +76,8 @@ const errorMsg = ref('')
 const isLoading = ref(false)
 
 const handleGoogleCallback = async (response) => {
+  console.log('RESPONSE GOOGLE:', JSON.stringify(response)) // Ditambahkan untuk pelacakan (debugging)
+
   errorMsg.value = '' // Clear previous errors
   isLoading.value = true
   try {
