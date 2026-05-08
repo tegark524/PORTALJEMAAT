9417a04d1a10d8885eba78431cfe5b62ba59cf8e
tegark524 <tegark525@gmail.com>
2026-05-08 15:57:32 +0700

    login new

--- src/views/admin/LoginView.vue
+++ src/views/admin/LoginView.vue
@@ -27,11 +27,7 @@
           <span class="ml-2 text-[#4e4e4e] font-medium text-[14px]">Memverifikasi akses...</span>
         </div>
         <div v-show="!isLoading">
-          <GoogleLogin
-            :callback="handleGoogleCallback"
-            popup-type="TOKEN"
-            scope="email profile openid"
-          >
+          <GoogleLogin :callback="handleGoogleCallback">
             <button class="button" type="button">
               <svg class="svg" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path
